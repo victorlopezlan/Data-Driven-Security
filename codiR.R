b222245e25798bdbdd23bbeb7b30d3e6afcdf666
opuigv <oriolpuigv@gmail.com>
2025-12-18 22:45:47 +0100

    Ejercicio 2.5 terminado

--- codiR.R
+++ codiR.R
@@ -58,3 +58,8 @@ epa_edu <- epa_edu[grepl("\\.txt", epa_edu$File), ]
 
 #Hacemos la suma de la columna de Bytes, ignorando aquellos que tienen valor de NA
 sum(epa_edu$Bytes, na.rm = T)
+
+################           Ejercicio 5                ########################
+
+#Hacemos la suma de las filas cuyo File tenga de valor "/"
+sum(epa_http$File == "/")
