8733b82677dfb8caccce4106ca72ce456fcd663d
Miriam Martínez Izquierdo <miriam.martinez.izquierdo@gmail.com>
2025-12-20 17:51:35 +0100

    Últimos cambios def

--- codiR.R
+++ codiR.R
@@ -1,41 +1,56 @@
-#--------------------------------
-# title: Práctica 1
+#----------------------------------
+# title: Práctica 1 - Ejercicio 2
 # author: Oriol, Víctor, Miriam
 # date: 2025-12-15
-#--------------------------------
+#----------------------------------
+
+#Configuración global
+knitr::opts_chunk$set(echo = F)
+knitr::opts_chunk$set(warning = F)
+knitr::opts_chunk$set(error = F)
+knitr::opts_chunk$set(message = F)
 
 # Carga de librerías
 library(readr)
 library(dplyr)
 library(stringr)
+
 # Carga de datos
 epa_http <- read_table("epa-http.csv", col_names = FALSE, show_col_types = F)
 colnames(epa_http) <- c("IP", "Timestamp", "Tipo", "URL", "Protocolo", "ResponseCode", "Bytes")
 # Cambiamos el tipo de la columna ResponseCode a factor 
 epa_http$ResponseCode <- as.factor(epa_http$ResponseCode)
 # Cambiamos el tipo de la columna Bytes a integer
-epa_http$Bytes <- as.integer(epa_http$Bytes)
+epa_http$Bytes <- as.integer(epa_http$Bytes, na.rm = T)
 #Limpiamos el simbolo " al inicio del campo Tipo con libreria stringr
 epa_http$Tipo <- str_remove(epa_http$Tipo, '^"')
 #Alternativa para limpiar " del inicio del campo Tipo
 #epa_http$Tipo <- sub('^"', '', epa_http$Tipo)
 #Limpiamos el simbolo " del final del campo Protocolo con libreria stringr
 epa_http$Protocolo <- str_remove(epa_http$Protocolo, '"$')
+
 # Visualización datos
-head(epa_http)
+# Tabla sencilla, únicamente muestra 6 filas
+knitr::kable(
+  head(epa_http),
+  caption = "Primeras 6 filas del conjunto de datos:"
+)
 
 ################           Ejercicio 1               ########################
+
 # Número filas
-nrow(epa_http)
+cat(nrow(epa_http))
+
 # Número columnas
-ncol(epa_http)
+cat(ncol(epa_http))
+
 # Valor medio de la columna Bytes
-mean(epa_http$Bytes, na.rm = T)
+cat(mean(epa_http$Bytes, na.rm = T))
 
 ################           Ejercicio 2               ########################
 
 # Número IPs .edu
-sum(grepl(".edu", epa_http$IP))
+cat(sum(grepl(".edu", epa_http$IP)))
 
 ################           Ejercicio 3               ########################
 
@@ -57,28 +72,36 @@ conteo_horas <- epa_get %>%count(Hora)
 hora_max <- conteo_horas %>%
   arrange(desc(n)) %>%  # Orden descendente
   slice(1) %>%                  # Solo la primera fila
-  pull(Hora)                     # Extrae la columna "Hora"
+  pull(Hora)                    # Extrae la columna "Hora"
 
 #Hora mayor volumen de peticiones
-hora_max
+cat(hora_max)
+
+#Tabla sencilla mostrando las 5 horas del dia con mayor numero de peticiones
+knitr::kable(
+  conteo_horas %>%
+    arrange(desc(n)) %>%  # Orden descendente
+    slice(1:5),           # 5 filas
+  caption = "Mostramos las 5 horas del día con mayor número de peticiones:"
+)
 
 ################           Ejercicio 4                ########################
 
-#filtramos solo las filas que contienen la cadena de texto ".edu" en la columna de IP
+#filtramos, filas que contienen ".edu" en la columna IP
 epa_edu <- epa_http[grepl("\\.edu", epa_http$IP), ]
 
 #filtramos solo las filas que contienen un fichero de descarga .txt
 epa_edu <- epa_edu[grepl("\\.txt", epa_edu$URL), ]
 
 #Hacemos la suma de la columna de Bytes, ignorando aquellos que tienen valor de NA
-sum(epa_edu$Bytes, na.rm = T)
+cat(sum(epa_edu$Bytes, na.rm = T))
 
 ################           Ejercicio 5                ########################
 
 #Hacemos la suma de las filas cuyo URL tenga de valor "/"
-sum(epa_http$URL == "/")
+cat(sum(epa_http$URL == "/", na.rm = T))
 
 ################           Ejercicio 6                ########################
 
-#Hacemos la suma de las filas cuyo Protocolo NO tenga de valor HTTP/0.2 
-cat(sum(epa_http$Protocolo != "HTTP/0.2"))
+#Suma filas Protocolo NO tenga valor HTTP/0.2 
+cat(sum(epa_http$Protocolo != "HTTP/0.2", na.rm = T))
